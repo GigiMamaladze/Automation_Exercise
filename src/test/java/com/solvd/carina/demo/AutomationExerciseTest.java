6737be8e447f4fc38361e0ad16dde949a09b9cc5
GigiMamaladze <gmamaladze@solvd.com>
2023-09-11 15:36:36 +0400

    Testing TSM

--- src/test/java/com/solvd/carina/demo/AutomationExerciseTest.java
+++ src/test/java/com/solvd/carina/demo/AutomationExerciseTest.java
@@ -3,6 +3,7 @@ package com.solvd.carina.demo;
 import com.solvd.carina.demo.gui.pages.common.HomePageBase;
 import com.solvd.carina.demo.gui.pages.common.LoginPageBase;
 import com.zebrunner.agent.core.annotation.Maintainer;
+import com.zebrunner.agent.core.annotation.TestCaseKey;
 import com.zebrunner.carina.core.IAbstractTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -11,6 +12,7 @@ public class AutomationExerciseTest implements IAbstractTest {
 
     @Test
     @Maintainer("Gmamaldze")
+    @TestCaseKey("DEF-1081")
     public void testLogin() {
         HomePageBase homepage  = initPage(getDriver(), HomePageBase.class);
         homepage.open();
